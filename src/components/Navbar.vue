3ac37e046ca6532fb674e8d86259ca15f3bd3d41
izulmk <izulmk06@gmail.com>
2023-07-01 14:54:39 +0800

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <nav class="navbar navbar-light fixed-top">
+    <div class="navbar-text ml-auto d-flex">
+      <button
+        class="btn btn-sm btn-outline-success"
+        @click="$parent.$emit('toggle-slide')"
+      >
+        <font-awesome-icon icon="dollar-sign" />
+      </button>
+      <div class="dropdown ml-2" v-if="cart.length > 0">
+        <button
+          class="btn btn-success btn-sm dropdown-toggle"
+          id="dropdownCart"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <span class="badge badge-pill badge-light">{{ cartQty }}</span>
+          <i class="fas fa-shopping-cart mx-2"></i>
+          <font-awesome-icon icon="shopping-cart" />
+          <price-display :value="Number(cartTotal)" />
+        </button>
+        <div
+          class="dropdown-menu dropdown-menu-right"
+          aria-labelledby="dropdownCart"
+        >
+          <div v-for="(item, index) in cart" :key="index">
+            <div class="dropdown-item-text text-nowrap text-right">
+              <span class="badge badge-pill badge-warning align-text-top mr-1">
+                {{ item.qty }}
+              </span>
+              {{ item.product.name }}
+              <b>{{ calculateTotal(item.qty, item.product.price) }}</b>
+              <a
+                href="#"
+                class="badge badge-danger text-white"
+                @click.stop="$emit('delete-item', index)"
+              >
+                -
+              </a>
+            </div>
+          </div>
+          <router-link
+            class="btn btn-sm btn-outline-info text-dark float-right mr-2"
+            to="/checkout"
+          >
+            Checkout
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import PriceDisplay from "./PriceDisplay.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "NavbarTag",
+  props: ["cart", "cartQty", "cartTotal"],
+  components: {
+    PriceDisplay,
+    FontAwesomeIcon,
+  },
+  methods: {
+    calculateTotal(qty, price) {
+      return "Rp" + (qty * price).toFixed(2);
+    },
+  },
+};
+</script>
---
+++ src/components/Products.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1>IDShop</h1>
+    <navbar
+      :cart="cart"
+      :cartQty="cartQty"
+      :cartTotal="cartTotal"
+      @toggle-slide="toggleSlider"
+      @delete-item="deleteItem"
+    ></navbar>
+    <price-slider
+      :sliderStatus="sliderStatus"
+      v-model:maximum="maximum"
+    ></price-slider>
+    <pre>
+            <!-- <div>{{ JSON.stringify(products, null, 2) }}</div> -->
+        </pre>
+    <product-list
+      :products="products"
+      :maximum="maximum"
+      @add-item="addItem"
+    ></product-list>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar.vue";
+import PriceSlider from "./PriceSlider.vue";
+import ProductList from "./ProductList.vue";
+
+export default {
+  name: "ProductsView",
+  props: [
+    "products",
+    "maximum",
+    "cart",
+    "cartQty",
+    "cartTotal",
+    "sliderStatus",
+    "sliderState",
+  ],
+  components: {
+    Navbar,
+    PriceSlider,
+    ProductList,
+  },
+  methods: {
+    toggleSlider: function () {
+      this.$emit("toggle");
+    },
+    addItem: function () {
+      this.$emit("add", item);
+    },
+    deleteItem: function (index) {
+      this.$emit("delete", index);
+    },
+  },
+};
+</script>
